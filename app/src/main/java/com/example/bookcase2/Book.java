b9761e3026b46e98e8f8d211e0eeede8e881a494
Kwamina Thompson <tug82420@temple.edu>
2019-04-03 20:59:47 -0400

    layouts for activities have been updated and Book class been implemented

--- app/src/main/java/com/example/bookcase2/Book.java
+++ app/src/main/java/com/example/bookcase2/Book.java
@@ -1,4 +1,45 @@
 package com.example.bookcase2;
 
-public class Book {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Book implements Parcelable {
+
+    private String author, title, book_cover_URL ,  published;
+    private int id;
+
+    public  Book(JSONObject jsonBook) throws JSONException {
+        this.title = (jsonBook.getString("title"));
+        this.author= (jsonBook.getString("author"));
+        this.book_cover_URL =(jsonBook.getString("book_cover_URL"));
+        this.published = (jsonBook.getString("published"));
+        this.id = (jsonBook.getInt("book_id"));
+    }
+
+    protected Book(Parcel in) {
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
 }
